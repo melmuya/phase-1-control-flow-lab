c99beed9e200a68cc49fe35cc609bac436ea63c8
Melchizedek <melchizedek.maranga@gmail.com>
2023-03-16 10:49:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber < 400) {
+    result = "This one is on me!";
+  }
+  else if (2000<someNumber&&someNumber<2500) {
+    result = "I will gladly take your thirty bucks."
+  }else{result='No can do.'}
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(nameOfCity){
+  return nameOfCity === "NYC" ?  "Ok, sounds good." :  "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(passengerTip){
+  switch (passengerTip) {
+    case passengerTip = "generous":
+      return "Thank you so much."
+      break;
+    case passengerTip = "not as generous":
+      return "Thank you.";
+      
+      break;
+
+    default:
+      return "Bye.";
+      break;
+  }
 }
